58eec65031a490a1592716215f71774fd896ae01
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-09 10:04:15 +0100

    adds testing for validating fika model & factory

--- app/models/fika.rb
+++ app/models/fika.rb
@@ -1,2 +1,3 @@
 class Fika < ApplicationRecord
+  validates_presence_of :start_date, :end_date
 end
--- spec/models/fika_spec.rb
+++ spec/models/fika_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Fika, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB Table' do
+    it { is_expected.to have_db_column :start_date }
+    it { is_expected.to have_db_column :end_date }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :start_date }
+    it { is_expected.to validate_presence_of :end_date }
+  end
+
+  describe 'Associations' do
+  end
+
+  describe 'FactoryBot' do
+    it 'is expected to have a valid Factory' do
+      expect(create(:fika)).to be_valid
+    end
+  end
 end
