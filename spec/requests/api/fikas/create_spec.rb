1c13a26479b43f0e212fd17ce2fba2473297fc0d
oliverochman <oliver.ochman@hotmail.com>
2021-11-10 10:43:15 +0100

    create initial req spec for fikas#create

---
+++ spec/requests/api/fikas/create_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe 'POST /api/fikas', type: :request do
+    subject { response }
+    let!(:participants) { 4.times { create(:participant) } }
+
+    describe 'successfully' do
+        before do
+            post '/api/fikas'            
+        end
+
+        it { is_expected.to have_http_status 201 }
+
+        it 'is expected to return a message' do
+            expect(response_json['message']).to eq 'Fikas successfully created!!!'
+        end
+
+        it 'is expected to create two fikas' do
+            expect(Fikas.all.count).to eq 6
+        end
+    end
+end
