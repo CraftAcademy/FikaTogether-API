974daa92d7726f897220b396fbf6c6982d2026d3
maxarvid <max.arvid.anderson@gmail.com>
2021-11-11 12:12:34 +0100

    tweaks seeds to include department and seniority

--- db/seeds/002_participants.rb
+++ db/seeds/002_participants.rb
@@ -1,6 +1,8 @@
-Participant.find_or_create_by(name: 'Fraser Hughes', email: 'fraser@ft.se', start_date: DateTime.parse("09/01/2009 17:00"), management: true)
-Participant.find_or_create_by(name: 'Max Anderson', email: 'max@ft.se', start_date: DateTime.parse("09/01/2009 17:00"), management: true)
-Participant.find_or_create_by(name: 'Dorian Buck', email: 'dorian@ft.se', start_date: DateTime.parse("09/01/2009 17:00"), management: true)
-Participant.find_or_create_by(name: 'August Enhager', email: 'august@ft.se', start_date: DateTime.parse("09/01/2009 17:00"), management: false)
-
-
+Participant.find_or_create_by(name: 'Fraser Hughes', email: 'fraser@ft.se',
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: true, department: 'HR', seniority: 3)
+Participant.find_or_create_by(name: 'Max Anderson', email: 'max@ft.se', start_date: DateTime.parse('09/01/2009 17:00'),
+                              management: true, department: 'Admin', seniority: 3)
+Participant.find_or_create_by(name: 'Dorian Buck', email: 'dorian@ft.se',
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: true, department: 'Development', seniority: 3)
+Participant.find_or_create_by(name: 'August Enhager', email: 'august@ft.se',
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: false, department: 'C-Suite', seniority: 3)
