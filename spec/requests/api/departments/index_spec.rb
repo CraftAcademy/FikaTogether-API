ae44cf6de6bcc10be185e8ad3ef7853ac0114684
Dorian Buck <dorianbuck@gmail.com>
2021-11-11 22:39:28 +0100

    Wip adds the sad path to test

--- spec/requests/api/departments/index_spec.rb
+++ spec/requests/api/departments/index_spec.rb
@@ -2,9 +2,8 @@ RSpec.describe 'GET /api/departments', type: :request do
   subject { response }
 
   describe 'when there are departments in the database' do
-    # let!(:departments) { create_list(:department, 3) }
-    # let!(:participant_1) { create(:participant) }
-
+    let!(:department) { create(:department) }
+    
 
     before do
       get '/api/departments'
@@ -14,6 +13,20 @@ RSpec.describe 'GET /api/departments', type: :request do
 
     it 'is expected that department has a name' do
       binding.pry
+      # expect(response_json['departments'].first['name']).to be_truthy
+    end
+
+  end
+
+  describe 'when there are no departments in the database' do
+    before do
+      get '/api/departments'
+    end
+
+    it { is_expected.to have_http_status 404 }
+
+    it 'is expected that there are no departments' do
+      expect(response_json['message']).to 'There are no departments in the database'
     end
   end
 end
