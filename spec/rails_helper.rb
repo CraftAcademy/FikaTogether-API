4b89567339123765c699490ccfcf08205e82a50f
oliverochman <oliver.ochman@hotmail.com>
2021-11-15 16:10:59 +0100

    start sad paths

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,20 +28,36 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include Shoulda::Matchers::ActiveRecord, type: :model
   config.include ResponseJSON
-  config.before do
+  config.before(sad_mock: false) do
     @token_response = WebMock.stub_request(:post, %r{www.googleapis.com/oauth2/v4/token.*})
-      .to_return(
-        status: 200,
-        body: file_fixture('token_response.json'),
-        headers: { content_type: 'application/json' }
-      )
+                             .to_return(
+                               status: 200,
+                               body: file_fixture('token_response.json'),
+                               headers: { content_type: 'application/json' }
+                             )
 
-      @create_event_response = WebMock.stub_request(:post, %r{www.googleapis.com/calendar/v3/calendars/.*})
-      .to_return(
-        status: 200,
-        body: file_fixture('create_event_response.json'),
-        headers: { content_type: 'application/json' }
-      )
+    @create_event_response = WebMock.stub_request(:post, %r{www.googleapis.com/calendar/v3/calendars/.*})
+                                    .to_return(
+                                      status: 200,
+                                      body: file_fixture('create_event_response.json'),
+                                      headers: { content_type: 'application/json' }
+                                    )
+  end
+
+  config.before(sad_mock: true) do
+    @sad_token_response = WebMock.stub_request(:post, %r{www.googleapis.com/oauth2/v4/token.*})
+                             .to_return(
+                               status: 401,
+                               body: file_fixture("unauthorized_token_response.json"),
+                               headers: { content_type: 'application/json' }
+                             )
+
+    @sad_create_event_response = WebMock.stub_request(:post, %r{www.googleapis.com/calendar/v3/calendars/.*})
+                                    .to_return(
+                                      status: 400,
+                                      body: file_fixture("bad_request_response.json"),
+                                      headers: { content_type: 'application/json' }
+                                    )
   end
 end
 
--- spec/services/invite_service_spec.rb
+++ spec/services/invite_service_spec.rb
@@ -1,15 +1,22 @@
 RSpec.describe 'InviteService', type: :service do
   let!(:fika) { create(:fika) }
 
-  it 'is expected to call google api auth service' do
-    expect(
-      a_request(:post, %r{https://www.googleapis.com/oauth2/v4/token})
-    ).to have_been_made.at_least_once
+  describe 'successfully', sad_mock: false do
+    it 'is expected to call google api auth service' do
+      expect(
+        a_request(:post, %r{https://www.googleapis.com/oauth2/v4/token})
+      ).to have_been_made.at_least_once
+    end
+  
+    it 'is expected to call google api calendar service', mock: true do
+      expect(
+        a_request(:post, %r{https://www.googleapis.com/calendar/v3/calendars/})
+      ).to have_been_made.at_least_once
+    end
+  
   end
 
-  it 'is expected to call google api calendar service' do
-    expect(
-      a_request(:post, %r{https://www.googleapis.com/calendar/v3/calendars/})
-    ).to have_been_made.at_least_once
+  describe "unsuccessfully", sad_mock: true do
+    
   end
 end
