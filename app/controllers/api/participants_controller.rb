6a5a55da3a223686a884a31f61737388eb61593f
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-16 18:17:17 +0100

    creates sad path for not being authorized

--- app/controllers/api/participants_controller.rb
+++ app/controllers/api/participants_controller.rb
@@ -1,14 +1,20 @@
 class Api::ParticipantsController < ApplicationController
-  
+  before_action :authenticate_admin!, only: :create
+
   def create
     participant = Participant.new(participant_params)
     participant.department = Department.find_by(name: params[:participant][:department])
     participant.save
 
-    render json: {message: 'You successfully added participant to the department.'}, status: 201
+    if participant.persisted?
+      render json: { message: 'You successfully added participant to the department.' }, status: 201
+    else
+      render json: { errors: participant.errors.full_messages.to_sentences }, status: 422
+    end
   end
 
   private
+
   def participant_params
     params.require(:participant).permit(:name, :email, :management, :start_date, :seniority)
   end
--- spec/requests/api/participants/create_spec.rb
+++ spec/requests/api/participants/create_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'POST /api/participants', type: :request do
   let(:credentials) { admin.create_new_auth_token }
 
   describe 'successful' do
-    let!(:department) { create(:department, name: "HR") }
+    let!(:department) { create(:department, name: 'HR') }
 
     before do
       post '/api/participants',
@@ -25,6 +25,26 @@ RSpec.describe 'POST /api/participants', type: :request do
 
     it 'is expected to create a new participant' do
       expect(Participant.count).to eq 1
+    end
+  end
+
+  describe 'unsuccessful' do
+    describe 'when the user is not authorized' do
+      before do
+        post '/api/participants',
+             params: { participant: { name: 'John Snow',
+                                      email: 'John@Fika.com',
+                                      start_date: '2000-01-01',
+                                      department: 'HR',
+                                      management: false,
+                                      seniority: 2 } }
+      end
+
+      it { is_expected.to have_http_status 401 }
+
+      it 'is expected to return a message that user cannot create fikas' do
+        expect(response_json['errors'].first).to eq 'You need to sign in or sign up before continuing.'
+      end
     end
   end
 end
