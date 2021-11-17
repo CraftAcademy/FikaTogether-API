03ef61767f4f21cf72548c41c4a01b17ffdbdff0
Dorian Buck <dorianbuck@gmail.com>
2021-11-17 18:43:04 +0100

    adds switch case statement in score_service

--- app/services/score_service.rb
+++ app/services/score_service.rb
@@ -6,20 +6,23 @@ module ScoreService
     delta = (participant_1.start_date - participant_2.start_date).to_i
     result = Math.sqrt(delta * delta).to_i
     difference_in_days = result / (60 * 60 * 24)
-
-    binding.pry
-    fika.fika_score = if difference_in_days <= 1825
+    fika.fika_score = case difference_in_days
+                      when 0..1825
                         [5, 5]
-                      elsif difference_in_days <= 3650
+                      when 1826..3650
                         if delta >= 0
                           [6, 4]
                         else
                           [4, 6]
                         end
-                      elsif delta >= 0
-                        [7, 3]
-                      else
-                        [3, 7]
+                      when 3651..Float::INFINITY
+                        if delta >= 0
+                          [7, 3]
+                        else
+                          [3, 7]
+                        end
                       end
+
+    fika.save
   end
 end
