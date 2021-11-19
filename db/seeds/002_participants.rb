94290e4598ed837f9ba0e7a61e349d59a647e609
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-19 15:48:36 +0100

    updates seeds and fixing test

--- db/seeds/002_participants.rb
+++ db/seeds/002_participants.rb
@@ -1,13 +1,12 @@
 Participant.find_or_create_by(name: 'Fraser Hughes', email: 'fraser@ft.se',
-                              start_date: DateTime.parse('09/01/2009 17:00'), management: true, seniority: 3).update(department: Department.find_by(name: 'Marketing'))
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: true, seniority: 3, fika_score: 25).update(department: Department.find_by(name: 'Marketing'))
 Participant.find_or_create_by(name: 'Max Anderson', email: 'max@ft.se', start_date: DateTime.parse('09/01/2009 17:00'),
-                              management: true, seniority: 3).update(department: Department.find_by(name: 'Human Resources'))
+                              management: true, seniority: 3, fika_score: 16).update(department: Department.find_by(name: 'Human Resources'))
 Participant.find_or_create_by(name: 'Dorian Buck', email: 'dorian@ft.se',
-                              start_date: DateTime.parse('09/01/2009 17:00'), management: true, seniority: 3).update(department: Department.find_by(name: 'Finance'))
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: true, seniority: 3, fika_score: 31).update(department: Department.find_by(name: 'Finance'))
 Participant.find_or_create_by(name: 'August Enhager', email: 'august@ft.se',
-                                start_date: DateTime.parse('09/01/2009 17:00'), management: false, seniority: 3).update(department: Department.find_by(name: 'Sales'))
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: false, seniority: 3, fika_score: 19).update(department: Department.find_by(name: 'Sales'))
 Participant.find_or_create_by(name: 'Thomas Ochman', email: 'Thomas@craft.se',
-                                  start_date: DateTime.parse('09/01/2009 17:00'), management: false, seniority: 3).update(department: Department.find_by(name: 'Information Technology'))
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: false, seniority: 3, fika_score: 9).update(department: Department.find_by(name: 'Information Technology'))
 Participant.find_or_create_by(name: 'Oliver Ochman', email: 'Oliver@craft.se',
-                                    start_date: DateTime.parse('09/01/2009 17:00'), management: false, seniority: 3).update(department: Department.find_by(name: 'Information Technology'))
-  
+                              start_date: DateTime.parse('09/01/2009 17:00'), management: false, seniority: 3, fika_score: 21).update(department: Department.find_by(name: 'Information Technology'))
--- db/seeds/004_fikas.rb
+++ db/seeds/004_fikas.rb
@@ -2,41 +2,47 @@ Fika.find_or_create_by(
   id: 1001,
   start_date: DateTime.parse('13/11/2021 13:00'),
   end_date: DateTime.parse('13/11/2021 13:30'),
-  similarity: 0.8024069261575878
+  similarity: 0.8024069261575878,
+  score: [5, 5]
 ).update(participants: [Participant.find_by(name:"Fraser Hughes"), Participant.find_by(name:"Max Anderson")])
 
 Fika.find_or_create_by(
   id: 1002,
   start_date: DateTime.parse('13/11/2021 13:00'),
   end_date: DateTime.parse('13/11/2021 13:30'),
-  similarity: 0.1065972861575878
+  similarity: 0.1065972861575878,
+  score: [7, 3]
 ).update(participants: [Participant.find_by(name:"Dorian Buck"), Participant.find_by(name:"August Enhager")])
 
 Fika.find_or_create_by(
   id: 1003,
   start_date: DateTime.parse('13/11/2021 13:00'),
   end_date: DateTime.parse('13/11/2021 13:30'),
-  similarity: 0.692613673575878
+  similarity: 0.692613673575878,
+  score: [4, 6]
 ).update(participants: [Participant.find_by(name:"Fraser Hughes"), Participant.find_by(name:"August Enhager")])
 
 Fika.find_or_create_by(
   id: 1004,
   start_date: DateTime.parse('17/11/2021 13:00'),
   end_date: DateTime.parse('17/11/2021 13:30'),
-  similarity: 0.346739261575878
+  similarity: 0.346739261575878,
+  score: [3, 7]
 ).update(participants: [Participant.find_by(name:"Fraser Hughes"), Participant.find_by(name:"Dorian Buck")])
 
 Fika.find_or_create_by(
   id: 1005,
   start_date: DateTime.parse('17/11/2021 13:00'),
   end_date: DateTime.parse('17/11/2021 13:30'),
-  similarity: 0.4035669261575878
+  similarity: 0.4035669261575878,
+  score: [5, 5]
 ).update(participants: [Participant.find_by(name:"Dorian Buck"), Participant.find_by(name:"Max Anderson")])
 
 Fika.find_or_create_by(
   id: 1006,
   start_date: DateTime.parse('17/11/2021 13:00'),
   end_date: DateTime.parse('17/11/2021 13:30'),
-  similarity: 0.2346165785975878
+  similarity: 0.2346165785975878,
+  score: [4, 6]
 ).update(participants: [Participant.find_by(name:"August Enhager"), Participant.find_by(name:"Max Anderson")])
 
