27bccbc09086a0944310398f5550d56cb09b6b56
Thomas <thomas@craftacademy.se>
2021-11-19 10:42:50 +0100

    WIP - no significant progress

--- app/services/schedule_service.rb
+++ app/services/schedule_service.rb
@@ -1,7 +1,16 @@
 module ScheduleService
   def self.match(fika_pairs, date, time_slots)
-    unique_participants_ids = fika_pairs.uniq {|pair| pair.pluck(:id)}
-    binding.pry
+
+    time_slots.each do |slot|
+      parsed_date = Date.parse(date).to_datetime
+      time = Time.parse(slot)
+      date_time = (parsed_date + time.seconds_since_midnight.seconds).to_datetime
+      current_pair = fika_pairs.sample
+      fika_pairs.slice!(fika_pairs.index(current_pair))
+      fika = Fika.create(start_date: date_time, end_date: date_time + 45.minutes, participants: current_pair)
+      binding.pry
+    end
+
     fika_objects = []
     fika_pairs.each do |fika|
       participants_ids = []
@@ -12,20 +21,17 @@ module ScheduleService
     end
 
     fika_objects.each do |fika|
-      parsed_date = Date.parse(date).to_datetime
-      time = Time.parse(time_slots.first)
+      
       date_time = (parsed_date + time.seconds_since_midnight.seconds).to_datetime
       fika[:start_date] = date_time
     end
 
     all_unique_fikas = []
-
+    unique_participants_ids = sorted_fika_pairs.flatten.pluck(:id).uniq
     unique_participants_ids.each do |unique_participant|
       unique_fikas = []
       fika_objects.select do |fika|
-        if fika[:participant_ids].include? unique_participant 
-          unique_fikas << fika
-        end
+        unique_fikas << fika if fika[:participant_ids].include? unique_participant
       end
       all_unique_fikas << unique_fikas
     end
